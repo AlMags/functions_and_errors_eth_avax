fbeb270c421b16a15c837233f8aaf21aafce12df
Gab Mags <seperatev2@gmail.com>
2023-10-11 07:56:04 +0800

    update

---
+++ contracts/Assessment.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract Assessment {
+
+    uint public sampleBalance = 5;
+
+    function aRequire(uint x) public pure{
+        require(x >= 5, "Must be greater than 5");
+    }
+
+    function aRevert(uint x) public pure{
+        if(x >= 10){
+            revert("Input is greater than or equal to 10");
+        }else{
+            revert("Input is less than 10");
+        }
+    }
+
+    function aAssert() public view returns (string memory){
+        assert(sampleBalance > 0);
+        return "Test Successful";
+    }
+
+}
